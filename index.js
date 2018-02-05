24d50d1b906f74799abe35a6e193e18b25488b34
Kevin Gorjan <kevin.gorjan@enervalis.com>
2018-02-05 15:12:09 +0100

    added fastify

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
+const fastify = require('../fastify')()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const schema = {
+	schema: {
+		response: {
+			200: {
+				type: 'object',
+				properties: {
+					hello: {
+						type: 'string'
+					}
+				}
+			}
+		}
+	}
+}
+
+fastify
+	.get('/', schema, function (req, reply) {
+		reply
+			.send({hello: 'world'})
+	})
+	.listen(PORT, err => {
+	if (err) throw err
+	console.log(`server listening on ${fastify.server.address().port}`)
+})
